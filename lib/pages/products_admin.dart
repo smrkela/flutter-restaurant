e5a973af74ec1020ef3fda9a5ad9c1d537ded532
Sasa Mrkela <sasa.mrkela@runsimply.com>
2019-01-20 23:57:34 +0100

    Further fixes

--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -47,7 +47,7 @@ class ProductsAdminPage extends StatelessWidget {
           body: TabBarView(
             children: <Widget>[
               ProductEditPage(),
-              ProductListPage(null, null, null),
+              ProductListPage(),
             ],
           )),
     );
